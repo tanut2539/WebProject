1c324b030ed8b431401c6ad4883b6db16158060e
TuyMove <veeravat@me.com>
2018-10-04 12:19:19 +0700

    Update new-post.php
    - Fix multiple open PHP tags
    - Combine all insert string
    - Prevent SQL Injection

--- includes/new-post.php
+++ includes/new-post.php
@@ -1,22 +1,25 @@
 <?php
-			if(isset($_POST['new-post']))
-			{
-				if(trim($_POST['forum-title'] && $_POST['text-details'] && $_POST['text-feeds']) == "")
-				{
-		?>
-					<div class="msg_error">กรุณากรอกข้อมูลให้ครบถ้วน</div>
-		<?php
-				}
-				else
-				{
-						date_default_timezone_set('Asia/Bangkok');
-						$up = $_SESSION["user_login"];
-						$insert = "INSERT INTO db_forums ";
-						$insert .="(createdate,user_post,title,details,details_index) ";
-						$insert .="VALUES ";
-						$insert .="('".date("Y-m-d H:i:s")."','$up','".$_POST["forum-title"]."','".$_POST["text-details"]."','".$_POST["text-feeds"]."')";
-						$query = mysql_query($insert);
-						header("location: index");
-				}
-			}
+        if(isset($_POST['new-post']))
+        {
+            if(trim($_POST['forum-title'] == "" && $_POST['text-details'] == "" && $_POST['text-feeds']) == "")
+            {
+                echo "<div class=\"msg_error\">กรุณากรอกข้อมูลให้ครบถ้วน</div>";
+            }
+            else
+            {
+                    date_default_timezone_set('Asia/Bangkok');
+                    $forumTitle = mysql_real_escape_string($_POST["forum-title"]);
+                    $textDetail = mysql_real_escape_string($_POST["text-details"]);
+                    $textFeed = mysql_real_escape_string($_POST["text-feeds"]);
+                    $up = $_SESSION["user_login"];
+                    $insert = "INSERT INTO db_forums 
+                                (createdate,user_post,title,details,details_index)
+                               VALUES
+                                ('".date("Y-m-d H:i:s")."','$up','".$forumTitle."',
+                                '".$textDetail."','".$textFeed."')
+                    ";
+                    $query = mysql_query($insert);
+                    header("location: index");
+            }
+        }
 ?>
